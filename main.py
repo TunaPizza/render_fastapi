ca14cd59edcd199522177faff3e60b3255529c74
TunaPizza <23FI031@ms.dendai.ac.jp>
2025-06-13 13:53:12 +0900

    change

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ def index():
 
 @app.post("/present")
 async def give_present(present):
-    return {"response": f"サーバです。 {present}ありがとう。とっても嬉しいです。"}  # f文字列というPythonの機能を使っている
+    return {"response": f"サーバです。 {present}をくれてありがとう。とっても嬉しいです。"}  # f文字列というPythonの機能を使っている
